e0d8e3823a76def75dea045dced6d70b108dc0b1
Sam Praneis <sampraneis@gmail.com>
2025-04-16 14:25:13 -0500

    debug statemtents

--- game-state/game-state.ino
+++ game-state/game-state.ino
@@ -9,6 +9,7 @@ Packet packetArr[48];
 int cooldownsExpired = 0b11111;
 
 void managePacket(Packet* packet) {
+  Serial.println("managing packet");
     switch (packet->getType()) {
         case PowerupActivatePacket: {
             CooldownsTriggeredData data;
@@ -25,11 +26,19 @@ void managePacket(Packet* packet) {
 }
 
 void handlePowerupActivation(const CooldownsTriggeredData& data) {
+    Serial.println("handlig powerup activation");
     int toActivate = 0;
     for (int i = 0; i < NUM_POWERUPS; i++){
+        Serial.print("checking powerup: ");
+        Serial.println(i);
       if ((data.packetsTriggered >> i) & (cooldownsExpired >> 1)) {
+        Serial.println("should be activated! ");
         toActivate = toActivate | (0b1 << i);
+        Serial.print("toActivate is now:");
+        Serial.println(toActivate);
         cooldownsExpired = cooldownsExpired ^ (0b1 << i);
+        Serial.print("cooldowns expired is now: ");
+        Serial.println(cooldownsExpired);
       }
     }
 
@@ -38,14 +47,19 @@ void handlePowerupActivation(const CooldownsTriggeredData& data) {
       data.packetsTriggered = toActivate;
       Packet p = Packet(PowerupActivatePacket).withData(&data).sendable();
 
+      Serial.println("sending packet to game input");
       server.sendPacket(GAME_IN, &p);
+      Serial.println("sending packet to powerup feedback");
       server.sendPacket(POWERUP_FB, &p);
     }
 }
 
 void handleCooldownExpired(const CooldownsExpiredData& data) {
+    Serial.println("handling cooldown expired");
     for (int i = 0; i < NUM_POWERUPS; i++){
       if ((data.cooldownsExpired >> i) & 0b1) {
+          Serial.print("cooldowns expired is now: ");
+          Serial.println(cooldownsExpired);
           cooldownsExpired = cooldownsExpired | (0b1 << i);
       }
     }
